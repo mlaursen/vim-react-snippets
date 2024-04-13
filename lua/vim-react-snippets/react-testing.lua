4e1f803e40e1c8c482b61c6b9cd7a304e4a75bf1
Mikkel Laursen <mlaursen03@gmail.com>
2024-04-13 17:02:24 -0400

    feat(luasnip): add jest gobals test snippet

--- lua/vim-react-snippets/react-testing.lua
+++ lua/vim-react-snippets/react-testing.lua
@@ -6,31 +6,69 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
+--- @param esm boolean
+--- @param globals boolean
+local react_test_file = function(esm, globals)
+  local rtl_index = (globals and 2) or 1
+  local filename_index = rtl_index + 1
+  local globals_import = globals
+      and {
+        t('import { describe, expect, it } from "'),
+        i(1, "@jest/globals"),
+        t({ '"', "" }),
+      }
+    or {}
+
+  return s(
+    {
+      trig = (globals and "g" or "r") .. "tf" .. (esm and "e" or ""),
+      name = (globals and "Globals" or "React") .. "Test File" .. (esm and " (ESM)" or ""),
+      desc = "Base react testing setup with react-testing-library"
+        .. (globals and " and jest globals" or "")
+        .. (esm and " and esm imports" or ""),
+    },
+    util.merge_lists(globals_import, {
+      t('import { render, screen, userEvent } from "'),
+      i(rtl_index, "@testing-library/react"),
+      t({ '"', "" }),
+      t("import "),
+      util.current_filename(filename_index),
+      t(' from "../'),
+      util.mirror_node(filename_index),
+      t(esm and ".js" or ""),
+      t({ '"', "", "" }),
+
+      t('describe("'),
+      util.mirror_node(filename_index),
+      t({ '", () => {', '\tit("should ' }),
+      i(filename_index + 1),
+      t({ '", () => {', "\t\t" }),
+      i(filename_index + 2),
+      t({ "", "\t})", "})" }),
+    })
+  )
+end
+
 --- @param typescript boolean
 --- @return unknown[]
 local react_testing = function(typescript)
   return {
-    s({
-      trig = "rtf",
-      name = "React Test File",
-      desc = "Base setup for a test in react with react-testing-library or a custom renderer",
-    }, {
-      t('import { render, screen } from "'),
-      i(1, "@testing-library/react"),
-      t({ '"', "" }),
-      t("import "),
-      util.current_filename(2),
-      t(' from "../'),
-      util.mirror_node(2),
-      t({ '"', "", "" }),
+    react_test_file(false, false),
+    react_test_file(false, true),
+    react_test_file(true, false),
+    react_test_file(true, true),
 
-      t('describe("'),
-      util.mirror_node(2),
-      t({ '", () => {', '\tit("should ' }),
-      i(3),
-      t({ '", () => {', "\t\t" }),
-      i(4),
-      t({ "", "\t})", "})" }),
+    s({
+      trig = "uet",
+      name = "User Event Test",
+    }, {
+      t('it("should '),
+      i(1),
+      t({ '", async () => {', "\t" }),
+      t({ "const user = userEvent.setup()", "\t" }),
+      i(2),
+      t({ "", "\texpect(true).toBe(true)" }),
+      t({ "", "})" }),
     }),
   }
 end
