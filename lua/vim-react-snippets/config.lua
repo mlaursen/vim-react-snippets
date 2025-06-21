32a01da67ecf604c20d372dc5b11cb3911fb680c
Mikkel Laursen <mlaursen03@gmail.com>
2025-06-21 10:31:09 -0400

    feat(test): try allowing configuring test framework and renderer path

--- lua/vim-react-snippets/config.lua
+++ lua/vim-react-snippets/config.lua
@@ -1,5 +1,16 @@
 return {
-  --- @type boolean Set to `false` if all props should no longer be wrapped in
+  --- Set to `false` if all props should no longer be wrapped in
   --- `Readonly<T>`.
+  --- @type boolean
   readonly_props = true,
+
+  --- @type string The test framework for the react test snippets. Can be set
+  --- to `"vitest"`
+  test_framework = "@jest/globals",
+
+  --- The import path for `import { render, screen, userEvent }`.
+  --- If you use a custom test renderer, set it to this config option.
+  --- i.e. `"@/test-utils.js"`
+  --- @type string
+  test_renderer_path = "@testing-library/react",
 }
